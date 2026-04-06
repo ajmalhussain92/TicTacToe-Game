8083e283280cb0af81c5c05fcb3c4bd38f503401
Md Ajmal Hussain <ajmalashrafi2016@gmail.com>
2026-04-07 02:57:56 +0530

    Sound added in the game

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-function showTime(){
-    document.getElementById("currentTime").innerHTML=new Date().toUTCString();
-}
-showTime();
-
-setInterval(function(){
-    showTime();
-});
-
 let boxes = document.querySelectorAll(".box");
 let newBtn = document.querySelector("#new-btn");
 let resetBtn = document.querySelector("#reset-btn");
@@ -29,12 +20,20 @@ const winPatterns = [
 ];
 
 boxes.forEach((box) => {
-    box.addEventListener('click',()=>{
-        if(turnO){
+    // Play start sound
+    let startSound = new Audio("Sound/Start.wav");
+    startSound.play();
+
+    box.addEventListener('click', () => {
+        // Play click sound
+        let clickSound = new Audio("Sound/Click.wav");
+        clickSound.play();
+
+        if (turnO) {
             box.textContent = "O";  //setting value
             box.disabled = true;
             turnO = false;
-        }else{
+        } else {
             box.textContent = "X";
             box.disabled = true;
             turnO = true;
@@ -43,17 +42,16 @@ boxes.forEach((box) => {
     })
 })
 
+// Reset Game
 const enableBoxes = () => {
-    for(let box of boxes){
+    // Play reset sound
+    let resetSound = new Audio("Sound/Reset.wav");
+    resetSound.play();
+
+    for (let box of boxes) {
         box.disabled = false;
         box.innerText = "";
         msgContainer.classList.add("hide");
-    }
-}
-
-const disableBoxes = () => {
-    for(let box of boxes){
-        box.disabled = true;
     }
 }
 
@@ -65,20 +63,18 @@ const showWinner = (winner) => {
 }
 
 const checkWinner = () => {
-    for(pattern of winPatterns){
-        console.log(pattern[0], pattern[1], pattern[2]);
-        console.log(
-            boxes[pattern[0]].innerText,
-            boxes[pattern[1]].innerText,
-            boxes[pattern[2]].innerText
-        );
+    for (pattern of winPatterns) {
         let pos1Val = boxes[pattern[0]].innerText;
         let pos2Val = boxes[pattern[1]].innerText;
         let pos3Val = boxes[pattern[2]].innerText;
 
-        if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
-            if(pos1Val === pos2Val && pos2Val === pos3Val){
+        if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
                 showWinner(pos1Val);
+                // Play win sound
+                let winSound = new Audio("Sound/Win.wav");
+                winSound.play();
+
                 box.disabled = true;
             }
         }
@@ -94,11 +90,4 @@ newBtn.addEventListener('click', () => {
     gmBx.classList.remove("hide");
     newBtn.classList.add("hide");
     enableBoxes();
-})
-
-// let color = "maroon";
-let bg_btn = document.querySelector(".bg-btn");
-
-bg_btn.addEventListener('click', ()=> {
-    body.classList.add("#them1");
 });
